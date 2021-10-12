b04d299d566fdbd22ef3f1df098bc3c33e559f1b
Jasper Coppens <jasper.coppens@student.odisee.be>
2021-10-12 10:22:48 +0200

    Refactored tab1 and tab 2 into home and favorites

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,25 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
-import './Tab2.css';
+import './Home.css';
 
-const Tab2: React.FC = () => {
+const Tab1: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Homepage</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
+            <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        <ExploreContainer name="Homepage" />
       </IonContent>
     </IonPage>
   );
 };
 
-export default Tab2;
+export default Tab1;
