3217d653485d4b5a8342e4d29414e23ee4b4f8c0
Jasper Coppens <jasper.coppens@student.odisee.be>
2021-10-29 03:38:24 +0200

    Added css for RecipePage.tsx

--- src/pages/RecipePage.tsx
+++ src/pages/RecipePage.tsx
@@ -1,16 +1,28 @@
-import {IonContent, IonFab, IonFabButton, IonHeader, IonIcon, IonPage, IonTitle, IonToolbar} from '@ionic/react';
+import {
+    IonContent,
+    IonFab,
+    IonFabButton,
+    IonHeader,
+    IonIcon,
+    IonPage,
+    IonText,
+    IonTitle,
+    IonToolbar
+} from '@ionic/react';
 import { db } from '../firebase/firebase.utils';
 import React, {useEffect, useState} from "react";
 import {useParams} from "react-router";
 import {Recipe, toRecipe} from "../models/recipe";
 import { chatbubble } from "ionicons/icons";
+import Header from "../components/Header";
+import styles from "./RecipePage.module.css";
 
 interface RouteParams {
     id: string;
 }
 
 const RecipePage: React.FC = () => {
-    console.log("recipe page load");
+
     const { id } = useParams<RouteParams>();
     const [recipe, setRecipe] = useState<Recipe>();
     useEffect(() => {
@@ -18,13 +30,16 @@ const RecipePage: React.FC = () => {
         recipeRef.get().then ((doc) => setRecipe(toRecipe(doc)));
     }, [id]);
     return (
-        <IonPage>
+        <IonPage >
             <IonHeader>
-                <IonToolbar>
-                    <IonTitle>{recipe?.title}</IonTitle>
-                </IonToolbar>
+                <Header />
             </IonHeader>
-            <IonContent fullscreen>
+            <IonContent class="ion-padding">
+                <IonText class="ion-text-center">
+                    <h2>{recipe?.title}</h2>
+                </IonText>
+                <p className={styles.uploader}>Door {recipe?.uploaderId}</p>
+                {/*<img src={recipe?.imagePath}/>*/}
                 <p>{recipe?.description}</p>
                 <IonFab vertical='bottom' horizontal='end' slot='fixed'>
                     <IonFabButton>
